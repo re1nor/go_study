e9329a5e02aeed568339c0247da5d0d154480530
Bulat Zamaletdinov <zamaletdinovbulat@yandex.ru>
2021-12-11 12:59:43 +0300

    Runes

---
+++ rune.go
@@ -0,0 +1,16 @@
+/*
+	Руны в языке Go представляют отдельные символы.
+	Рунные литералы записываютсяв одиночных кавычках (').
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println('A')
+	fmt.Println('B')
+	fmt.Println('C')
+	fmt.Println('ү')
+	fmt.Println('Ә')
+	fmt.Println('\n')
+}
