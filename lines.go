0c87dbb7ab08176bb78cfb92948174866c1ec12d
Bulat Zamaletdinov <zamaletdinovbulat@yandex.ru>
2021-12-11 11:07:44 +0300

    Lines

---
+++ lines.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Go!")
+	// \n Символ новой строки
+	fmt.Println("Hello, \nGo!")
+
+	// \t  Символ табуляции
+	fmt.Println("Hello, \tGo!")
+
+	// \" Двочная ковычка
+	fmt.Println("Hello, \"Go!\"")
+
+	// \\ Обратный слэш
+	fmt.Println("Hello, \\Go!")
+}
