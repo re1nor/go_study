a72102b4e4bbdaa75cb3c0043dd94e650c68a239
Bulat Zamaletdinov <zamaletdinovbulat@yandex.ru>
2021-12-11 10:30:02 +0300

    Println function

---
+++ Println.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Несколько аргументов
+	fmt.Println("First", "Second", "Third")
+	// Без аргументов
+	fmt.Println()
+	// Переход на новую строку
+	fmt.Println("New line")
+}
