e30523153615621e23c4f6387b7ed94ce02215a4
Bulat Zamaletdinov <zamaletdinovbulat@yandex.ru>
2021-12-13 12:09:19 +0300

    Zero values

---
+++ zero_values.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myInt int
+	var myFloat float64
+	var myBool bool
+	var myString string
+
+	fmt.Println("myInt:", myInt, " myFloat:", myFloat)
+	fmt.Println("myBool:", myBool, " myString:", myString)
+}
