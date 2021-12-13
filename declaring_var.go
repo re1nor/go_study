431ee337be16cd9cfc945523fac754cc707af246
Bulat Zamaletdinov <zamaletdinovbulat@yandex.ru>
2021-12-13 11:45:15 +0300

    Declaring variables
    Declaring variables, assigning values to them, and using

---
+++ declaring_var.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	//	Объявление переменных:
+	//	Ключевое слово "var" имя_переменной тип
+	var name string
+	var year, month, day int
+	var education bool
+	// Объявление переменной и присвоение ей значения в одной строке
+	var weight = 58.2
+
+	// Присвоение значений переменным
+	name = "bulat"
+	year, month, day = 2001, 12, 26
+	education = true
+
+	// Использлвание переменных
+	fmt.Println("Имя гражданина: ", strings.Title(name))
+	fmt.Println("Дата рождения:", day, ".", month, ".", year)
+	fmt.Println("Наличие образования:", education)
+	fmt.Println("Вес:", weight)
+}
